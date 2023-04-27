42ceb63a4c289ff81bafb4f20ec2dddf1f80195f
FelippePacomio <felipepacomio@gmail.com>
2023-04-26 22:47:47 -0300

    Criação da estrutura do programa e das telas da interface gráfica.

--- PI_Wedding-Interface/src/br/weddinginterface/model/Conjuge.java
+++ PI_Wedding-Interface/src/br/weddinginterface/model/Conjuge.java
@@ -5,3 +5,5 @@ public class Conjuge {
     private String nome, dataCerimonia, dataRecepcao, cerimonialista, anotacoes;
     private double valorCerimonialista, valorGasto, orcamentoPrevisto, acessorios;
 }
+
+// teste github
